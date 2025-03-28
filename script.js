c7a75e80715e1f4635fb89b9ac9184edd42db6b1
ErickGrisoste <grisosteerick25@gmail.com>
2025-03-28 20:25:40 -0300

    outras operacoes - * /

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-function somar(){
-    let num1 = parseFloat(document.getElementById('num1').value);
-    let num2 = parseFloat(document.getElementById('num2').value);
+function lerNum1(){
+    var num1 = parseFloat(document.getElementById('num1').value);
+    return num1;
+}
 
-    let resultado = num1 + num2;
+function lerNum2(){
+    var num2 = parseFloat(document.getElementById('num2').value);
+    return num2;
+}
+
+function somar(){
+    let resultado = lerNum1() + lerNum2();
     document.getElementById('resultado').innerText = resultado;
 }
+
+function subtrair(){
+    let resultado = lerNum1() - lerNum2();
+    document.getElementById('resultado').innerText = resultado;
+}
+
+function multiplicar(){
+    let resultado = lerNum1() * lerNum2();
+    document.getElementById('resultado').innerText = resultado;
+}
+
+function dividir(){
+    let resultado = lerNum1() / lerNum2();
+
+    if(lerNum2() !== 0){
+        document.getElementById('resultado').innerText = resultado;
+    } else{
+        document.getElementById('resultado').innerText = "!!!";
+    }
+
+    
+}
+
